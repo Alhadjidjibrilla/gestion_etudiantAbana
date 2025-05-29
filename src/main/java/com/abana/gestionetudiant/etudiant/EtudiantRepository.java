39ce13aebb0b70bb12d0de55131db764493cbf53
leonel ka <waboleonel@gmail.com>
2025-05-29 12:40:08 +0100

    Definition du repository

---
+++ src/main/java/com/abana/gestionetudiant/etudiant/EtudiantRepository.java
@@ -0,0 +1,8 @@
+package com.abana.gestionetudiant.etudiant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
+}
