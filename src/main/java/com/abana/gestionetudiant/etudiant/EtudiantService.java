e19776ae556d1f0b3960381b7ec28aa352ad773f
leonel ka <waboleonel@gmail.com>
2025-06-03 16:37:14 +0100

    Definition de l'interface service

--- src/main/java/com/abana/gestionetudiant/etudiant/EtudiantService.java
+++ src/main/java/com/abana/gestionetudiant/etudiant/EtudiantService.java
@@ -1,8 +1,11 @@
 package com.abana.gestionetudiant.etudiant;
 
 import com.abana.gestionetudiant.etudiant.dto.EtudiantReponse;
+import com.abana.gestionetudiant.etudiant.dto.EtudiantRequete;
+
+import java.util.List;
 
 public interface EtudiantService {
-    EtudiantReponse getEtudiantById(Long id);
-    EtudiantReponse create(Etudiant newEtudiant);
+    EtudiantReponse create(EtudiantRequete requete);
+    List<EtudiantReponse> tousLesEtudiants();
 }
