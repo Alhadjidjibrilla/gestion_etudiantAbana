674534c35c7c6be21063c4202ffc8f9c7169f421
leonel ka <waboleonel@gmail.com>
2025-06-03 16:31:55 +0100

    Definition de l'exception pour les cas de mail deja pris

---
+++ src/main/java/com/abana/gestionetudiant/exceptions/MailDejaUtiliserException.java
@@ -0,0 +1,7 @@
+package com.abana.gestionetudiant.exceptions;
+
+public class MailDejaUtiliserException extends RuntimeException{
+    public MailDejaUtiliserException(String message) {
+        super(message);
+    }
+}
