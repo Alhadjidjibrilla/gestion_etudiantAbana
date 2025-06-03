a79d3cebc640340a8877077620303958b1b35886
leonel ka <waboleonel@gmail.com>
2025-06-03 16:32:38 +0100

    Definition de la mothode de recuperation d'etudiant par son mail

--- src/main/java/com/abana/gestionetudiant/etudiant/EtudiantRepository.java
+++ src/main/java/com/abana/gestionetudiant/etudiant/EtudiantRepository.java
@@ -3,6 +3,9 @@ package com.abana.gestionetudiant.etudiant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EtudiantRepository extends JpaRepository<Etudiant, Long> {
+    Optional<Etudiant> findByMailIgnoreCase(String mail);
 }
