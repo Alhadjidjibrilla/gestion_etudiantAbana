531592f3c85ae749be876d4ec6880b2a9f7ad3b2
leonel ka <waboleonel@gmail.com>
2025-06-10 20:11:20 +0100

    Definition de l'exception ElementIntrouvableException

---
+++ src/main/java/com/abana/gestionetudiant/exceptions/ElementIntrouvableException.java
@@ -0,0 +1,7 @@
+package com.abana.gestionetudiant.exceptions;
+
+public class ElementIntrouvableException extends RuntimeException{
+    public ElementIntrouvableException(String message) {
+        super(message);
+    }
+}
